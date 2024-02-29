ef3341d19e571a77cee345cd5a74c004b6cd66d2
Christopher Angoe <christopher.angoe@amalitech.com>
2024-02-29 11:08:08 +0000

    created bonus tranche table

--- amalisov.cuibono/webapp/model/formatter.ts
+++ amalisov.cuibono/webapp/model/formatter.ts
@@ -1,5 +1,15 @@
+import DateFormat from "sap/ui/core/format/DateFormat";
+
 export default {
 	formatValue: (value: string) => {
 		return value?.toUpperCase();
-	}
+	},
+
+	formatDate: (sDate: string): string => {
+        if (sDate) {
+            const oDateFormat = DateFormat.getDateInstance({ pattern: "MM.dd.yyyy" });
+            return oDateFormat.format(new Date(sDate));
+        }
+        return sDate;
+    }
 };
