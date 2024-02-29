013e8d3a13b9508be1b4fb1629b3e1bb46660df9
Christopher Angoe <christopher.angoe@amalitech.com>
2024-02-29 14:53:55 +0000

    added edit bonus tranch view

---
+++ amalisov.cuibono/webapp/controller/EditBonusTranche.controller.ts
@@ -0,0 +1,9 @@
+import MessageBox from "sap/m/MessageBox";
+import BaseController from "./BaseController";
+
+/**
+ * @namespace amalisov.cuibono.controller
+ */
+export default class EditBonusTranche extends BaseController {
+	
+}
--- amalisov.cuibono/webapp/controller/Main.controller.ts
+++ amalisov.cuibono/webapp/controller/Main.controller.ts
@@ -1,11 +1,10 @@
-import MessageBox from "sap/m/MessageBox";
 import BaseController from "./BaseController";
 
 /**
  * @namespace amalisov.cuibono.controller
  */
 export default class Main extends BaseController {
-	public sayHello(): void {
-		MessageBox.show("Hello World!");
+	public onCreateNewTranche(): void {
+		this.getRouter().navTo("EditBonusTranche");
 	}
 }
