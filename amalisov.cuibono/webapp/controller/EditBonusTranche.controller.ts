9f7df8026951eca4f9b05b066dd6211273afe601
Christopher Angoe <christopher.angoe@amalitech.com>
2024-03-01 15:42:09 +0000

    added filterbar and dymnic page in main view

--- amalisov.cuibono/webapp/controller/EditBonusTranche.controller.ts
+++ amalisov.cuibono/webapp/controller/EditBonusTranche.controller.ts
@@ -1,9 +1,16 @@
-import MessageBox from "sap/m/MessageBox";
+import Dialog from "sap/m/Dialog";
 import BaseController from "./BaseController";
 
 /**
  * @namespace amalisov.cuibono.controller
  */
 export default class EditBonusTranche extends BaseController {
-	
+	public onAddTarget(): void{
+        const oDialog = this.byId("editDialog") as Dialog;
+        oDialog.open();
+    }
+    public closeAddTarget(): void{
+        const oDialog = this.byId("editDialog") as Dialog;
+        oDialog.close();
+    }
 }
