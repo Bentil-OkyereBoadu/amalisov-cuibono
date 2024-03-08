c39dc7bc09f3652320686c833c3319c46cf779dc
Christopher Angoe <christopher.angoe@amalitech.com>
2024-03-08 16:29:41 +0000

    removed duplicate functions & added icontabheader for navigation

--- amalisov.cuibono/webapp/controller/App.controller.ts
+++ amalisov.cuibono/webapp/controller/App.controller.ts
@@ -1,6 +1,7 @@
 import BaseController from "./BaseController";
 import Button from "sap/m/Button";
 import HashChanger from "sap/ui/core/routing/HashChanger";
+import IconTabFilter from "sap/m/IconTabFilter";
 
 /**
  * @namespace amalisov.cuibono.controller
@@ -9,35 +10,41 @@ export default class App extends BaseController {
 	public onInit(): void {
 		// apply content density mode to root view
 		const oRouter = this.getOwnerComponent().getRouter();
-		this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+		this.getView().addStyleClass(
+			this.getOwnerComponent().getContentDensityClass()
+		);
 		// eslint-disable-next-line @typescript-eslint/unbound-method
-		oRouter.attachRouteMatched(this.onRouteMatched, this);
+
+		// oRouter.attachRouteMatched(this.onRouteMatched, this);
 	}
 
-	public onRouteMatched(): void {
-		const oView = this.getView()
+	// public onRouteMatched(): void {
+	// 	const oView = this.getView()
 
-		const oRouteButton = oView.byId("btnRoute") as Button; 
-		const oRouteButton2 = oView.byId("btnRoute1") as Button;  
+	// 	const oRouteButton = oView.byId("bonusNav") as IconTabFilter;
+	// 	const oRouteButton2 = oView.byId("participantNav") as IconTabFilter;
 
-		const sCurrentHash = HashChanger.getInstance().getHash();
+	// 	const sCurrentHash = HashChanger.getInstance().getHash();
 
-		if (!sCurrentHash) {
-			oRouteButton.addStyleClass("activeButton");
-			oRouteButton2.removeStyleClass("activeButton");
-		} else if (sCurrentHash === "participant") {
-			oRouteButton2.addStyleClass("activeButton");
-			oRouteButton.removeStyleClass("activeButton");
+	// 	if (!sCurrentHash) {
+	// 		oRouteButton.addStyleClass("activeButton");
+	// 		oRouteButton2.removeStyleClass("activeButton");
+	// 	} else if (sCurrentHash === "participant") {
+	// 		oRouteButton2.addStyleClass("activeButton");
+	// 		oRouteButton.removeStyleClass("activeButton");
+	// 	}
+	// }
+
+	public onRouteSelection(oEvent: any): void {
+		const selectedKey: string = oEvent.getSource().getSelectedKey();
+		console.log(selectedKey);
+
+		if (selectedKey === "bonus") {
+			const oRouter = this.getOwnerComponent().getRouter();
+			oRouter.navTo("main");
+		} else if (selectedKey === "participant") {
+			const oRouter = this.getOwnerComponent().getRouter();
+			oRouter.navTo("participants");
 		}
-	}
-
-	public onNavToRoute1(): void {
-		const oRouter = this.getOwnerComponent().getRouter();
-		oRouter.navTo("main");
-	}
-
-	public onNavToRoute2(): void {
-		const oRouter = this.getOwnerComponent().getRouter();
-		oRouter.navTo("participants");
 	}
 }
--- amalisov.cuibono/webapp/controller/EditBonusTranche.controller.ts
+++ amalisov.cuibono/webapp/controller/EditBonusTranche.controller.ts
@@ -11,23 +11,9 @@ export default class EditBonusTranche extends BaseController {
 
     public onInit(): void {
         const oRouter = this.getOwnerComponent().getRouter();
-        oRouter.getRoute("EditBonusTranche").attachPatternMatched(this.onRouteMatched, this);
         oRouter.getRoute("CreateTranche").attachPatternMatched(this.onCreateRoute, this);
         this.getView().setModel(this.getOwnerComponent().getModel("tranches"));
     };
-
-    public onRouteMatched(oEvent: any): void {
-        const oView = this.getView();
-        const oModel = this.getModel("updateModel");
-       
-        (oView.byId("nameInput") as Input).setValue(oModel.getProperty("/TrancheName"));
-        (oView.byId("locationSelect") as Input).setValue(oModel.getProperty("/Location"));
-        (oView.byId("startDateInput") as Input).setValue(formatter.formatDate(oModel.getProperty("/StartDate")));
-        (oView.byId("endDateInput") as Input).setValue(formatter.formatDate(oModel.getProperty("/EndDate")));
-        (oView.byId("weightInput") as Input).setValue(oModel.getProperty("/TrancheWeight"));
-        (oView.byId("descriptionInput") as Input).setValue(oModel.getProperty("/Description"));
-        (oView.byId("originDateInput") as Input).setValue(formatter.formatDate(oModel.getProperty("/OriginDate")));        
-    }
 
     public onCreateRoute(oEvent: any): void {
         const oView = this.getView();
