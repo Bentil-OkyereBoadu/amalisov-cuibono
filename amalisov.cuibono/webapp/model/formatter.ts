2ebce5c2bc9e8bb52f353a381c621d523c1840fb
Benjamin Cyubahiro <benjamin.cyubahiro@amalitech.com>
2024-03-08 08:56:03 +0200

    created participant page

--- amalisov.cuibono/webapp/model/formatter.ts
+++ amalisov.cuibono/webapp/model/formatter.ts
@@ -1,5 +1,8 @@
 export default {
 	formatValue: (value: string) => {
 		return value?.toUpperCase();
-	}
+	},
+	statusEnabled(status: string): boolean {
+        return status !== "Completed";
+    }
 };
