f0871d810f8ed59d3407f21a33d9efbd7de2a688
zachee ishimwe <zacheeishimwe@gmail.com>
2024-03-28 13:26:50 +0200

    fix the transaction issue during creating of bonus trench

--- src/handler/bonusTrencheService/createBonusTranche.handler.ts
+++ src/handler/bonusTrencheService/createBonusTranche.handler.ts
@@ -1,12 +1,6 @@
 import { Service } from "typedi";
 import { Handler, Srv, Req, Action } from "cds-routing-handlers";
 import { Request } from "@sap/cds";
-// import {BonusTranche} from '../../../@cds-models/amalisov/cuibono/bonusTranche';
-// import {Target} from '../../../@cds-models/amalisov/cuibono/targetAmount';
-// import { Employees } from "../../../@cds-models/amalisov/cuibono/employee";
-// import { TrancheParticipation } from "../../../@cds-models/amalisov/cuibono/trancheParticipation";
-
-// const { createTranche } = require('#cds-models/BonusTranche')
 const { BonusTranche } = require('#cds-models/amalisov/cuibono/bonusTranche')
 const { Target } = require('#cds-models/amalisov/cuibono/targetAmount')
 const { Employees } = require('#cds-models/amalisov/cuibono/employee')
@@ -14,30 +8,31 @@ const { TrancheParticipation } = require('#cds-models/amalisov/cuibono/tranchePa
 
 @Service()
 @Handler()
-export class CreateTrancheHandler{
+export class CreateTrancheHandler {
     @Action('createTranche')
-    public async newTranche(@Srv() srv:any,@Req() req:Request){
-        const {name,startDate,endDate,weight, Status,location,targets} = req.data
-        const [result] = await INSERT.into(BonusTranche.name).entries({ name , startDate, endDate,Status, location});
-        if(targets.length > 0){
-          targets.forEach(async(d: { name: any; weight: any; achievement: any; description: any; }) => {
-            const {name,weight,achievement,description} = d
-            const [query] = await INSERT.into(Target.name).entries({
-                name,
-                weight,
-                achievement,
-                description,
-                bonusTranche_ID: result.ID
-            })
-             return query
-            })
+    public async newTranche(@Srv() srv: any, @Req() req: Request) {
+        const { name, startDate, endDate, weight, Status, location, targets } = req.data;
+        const [result] = await INSERT.into(BonusTranche.name).entries({ name, startDate, endDate, Status, location });
+        console.log(result);
+        if (targets.length > 0) {
+            for (let i = 0; i < targets.length; i++) {
+                const { name, weight, achievement, description } = targets[i];
+                const [query] = await INSERT.into(Target.name).entries({
+                    name,
+                    weight,
+                    achievement,
+                    description,
+                    bonusTranche_ID: result.ID
+                });
+            }
         }
-        const data= await SELECT.from(Employees.name)
 
-        data.forEach(async(d: { firstName: any; lastName: any; department_ID: any; }) => {
-            const {firstName, lastName, department_ID} = d
+        const data = await SELECT.from(Employees.name);
+
+        for (let i = 0; i < data.length; i++) {
+            const { firstName, lastName, department_ID } = data[i];
             const [query] = await INSERT.into(TrancheParticipation.name).entries({
-                name: {firstName,lastName},
+                name: `${firstName} ${lastName}`,
                 Status,
                 startDate,
                 endDate,
@@ -45,9 +40,9 @@ export class CreateTrancheHandler{
                 location,
                 department_ID,
                 bonusTranche_ID: result.ID
-            })
-            return query
-        })
-        return result
+            });
+        }
+
+        return result;
     }
 }
