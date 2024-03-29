4dd1360cb4a280b0c974937979846b66768a7878
zachee ishimwe <zacheeishimwe@gmail.com>
2024-03-29 12:21:26 +0200

    implimented the requested changes

--- src/handler/bonusTrencheService/updateBonusTrench.handler.ts
+++ src/handler/bonusTrencheService/updateBonusTrench.handler.ts
@@ -15,36 +15,40 @@ export class UpdateBonusTrancheHandler {
     @Next() next: any
   ): Promise<any> {
     logger.info("Read Handler of BonusTrench");
-    const { ID, name, weight, startDate, endDate, location,targets } = req.data;
+    const { ID, name, weight, startDate, endDate, location,targets,Status } = req.data;
     const bonusTranche = await SELECT.one.from(BonusTranche.name).where({ ID });
     if (!bonusTranche) return req.reject(404, "Tranche not found");
-    if (bonusTranche.Status !== "Open") return req.reject(400, `BonusTranche cannot be updted while the status is ${bonusTranche.status}`)
+    if (bonusTranche.Status) await UPDATE(BonusTranche, ID).with({Status});
+    if (bonusTranche.Status === "Open"){
+      const data = {
+        name: name ? name : bonusTranche.name,
+        weight: weight ? weight : bonusTranche.weight,
+        startDate: startDate ? startDate : bonusTranche.startDate,
+        endDate: endDate ? endDate : bonusTranche.endDate,
+        location: location ? location : bonusTranche.location
+      };
+     if(targets.length > 0){
+      await DELETE.from(Target.name).where({bonusTranche_ID:ID})
+      await Promise.all(targets.map(async(target:any) => {
+        const {name,weight,achievement,description} = target
+        await INSERT.into(Target.name).entries({
+          name,
+          weight,
+          achievement,
+          description,
+          bonusTranche_ID:ID
+        })
+      }))
+     }
+      await UPDATE(BonusTranche, ID).with({ ...data });
+      return {
+        code: 200,
+        message: "Tranche udpated successfully",
+        status: "SUCCESS",
+      };
+    }else{
+      return req.reject(400, `BonusTranche cannot be updted while the status is ${bonusTranche.Status}`)
+    }
     
-    const data = {
-      name: name ? name : bonusTranche.name,
-      weight: weight ? weight : bonusTranche.weight,
-      startDate: startDate ? startDate : bonusTranche.startDate,
-      endDate: endDate ? endDate : bonusTranche.endDate,
-      location: location ? location : bonusTranche.location
-    };
-   if(targets.length > 0){
-    await DELETE.from(Target.name).where({bonusTranche_ID:ID})
-    await Promise.all(targets.map(async(target:any) => {
-      const {name,weight,achievement,description} = target
-      await INSERT.into(Target.name).entries({
-        name,
-        weight,
-        achievement,
-        description,
-        bonusTranche_ID:ID
-      })
-    }))
-   }
-    await UPDATE(BonusTranche, ID).with({ ...data });
-    return {
-      code: 200,
-      message: "Tranche udpated successfully",
-      status: "SUCCESS",
-    };
   }
 }
